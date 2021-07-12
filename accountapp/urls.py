003c680870d32541fbe802502ce52a1bcfb0f88b
jin2313 <ljh8190@naver.com>
2021-07-12 15:19:39 +0900

    07/12 web 01 commit2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from accountapp.views import hello_world
 from django.urls import path
 
-appname = 'accountapp'
+app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world')
