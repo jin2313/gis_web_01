5c3285b69abdb4eab71415253581d1dbcc87f39f
jin2313 <ljh8190@naver.com>
2021-07-19 14:55:49 +0900

    07/19 web 01 commit1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 from django.urls import path
 
 app_name = 'accountapp'
@@ -9,7 +9,8 @@ urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'), # 경로, 뷰, 이름 순서로 적기
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('create/', AccountCreateView.as_view(), name='create'), # 경로, 함수(클래스일 때 as.view() 사용), 이름
+    path('create/', AccountCreateView.as_view(), name='create'), # 경로, 로직 함수(클래스일 때 as.view() 사용), 이름
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
-    path('update/<int:pk>', AccountUpdateView.as_view(), name='update')
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete')
 ]
