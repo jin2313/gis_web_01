c22d4f80a9fddb4183462ce796bda3bd71967be1
jin2313 <ljh8190@naver.com>
2021-07-26 16:14:32 +0900

    07/26 web 01 commit4

--- practice_01/urls.py
+++ practice_01/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')), # 주소창에 이거를 치면 accounts에서 하위 분기를 한다는 뜻
     path('profile/', include('profileapp.urls')) # 여기 적힌 url이 profileapp의 urls에 적힌 app_name을 뜻함
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # 개발 환경에서 이미지를 제공해 줄 수 있는 코드 (배포용에 사용하면 안 됨)
