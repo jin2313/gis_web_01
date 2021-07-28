09998e4614cc57fe46e90df290db1caed5e9726d
jin2313 <ljh8190@naver.com>
2021-07-28 09:34:54 +0900

    07/28 web 01 commit1

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreateView
+from profileapp.views import ProfileCreateView, ProfileUpdateView
 
 app_name = 'profileapp'
 
 urlpatterns = [
-    path('create/', ProfileCreateView.as_view(), name='create')
+    path('create/', ProfileCreateView.as_view(), name='create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update')
+    # 제일 처음에 mainapp의 url에서 update로 분기했기 때문에 각 앱 안에서 이름이 겹쳐도 괜찮
 ]
