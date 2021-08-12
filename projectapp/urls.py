662bde61bc277c8a239d4b7777a2d68df336e815
jin2313 <ljh8190@naver.com>
2021-08-12 15:54:20 +0900

    08/12 web 01 commit6

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,5 +1,10 @@
-app_name = 'projectapp'
+from django.urls import path
+
+from projectapp.views import ProjectCreateView
+
+app_name = 'projectapp' # reverse 쓸 때 사용하는 것
 
 urlpatterns = [
+    path('create/', ProjectCreateView.as_view(), name='create'),
 
 ]
