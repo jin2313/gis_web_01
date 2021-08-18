ff0e99e904fd35e455d0eb3c1f400504ba918bf5
jin2313 <ljh8190@naver.com>
2021-08-18 10:12:40 +0900

    08/18 web 01 commit4

--- articleapp/models.py
+++ articleapp/models.py
@@ -2,11 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
     # ForeignKey: 1대다로 연결해 주는 것 -> 한사람이 게시글 하나만 쓸 수 있는 것은 아니기 때문에
     # related_name: 유저와 연결되어 있는 article을 접근할 때 쓰는 이름
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
+    # related_name: 게시에서 게시글을 접근할 때 사용하는 것
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
     # upload_to: media 폴더 내에 article이라는 폴더를 새로 생성해 거기에 이미지 저장
