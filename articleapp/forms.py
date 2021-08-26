657177056c623bd3baf22805b7a5ff787ad814cb
jin2313 <ljh8190@naver.com>
2021-08-26 14:55:56 +0900

    08/26 web 01 commit1

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,11 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
 
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable', 'style': 'text-align: left;' 'min-height: 10rem;'})) # 클래스를 적용하여 medium editor가 동작하도록 함
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
